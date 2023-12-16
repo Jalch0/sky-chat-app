ba9b8450d6b8a051e6c2660685c4e3ab75319e45
Jalch0 <jlabradorchacon@gmail.com>
2023-12-16 18:55:38 -0400

    Social media login and add toast errors

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import axios from "axios";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import Button from "@/app/components/Button";
 import Input from "@/app/components/input/input";
 import { useCallback, useState } from "react";
 import { FieldValue, FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -37,18 +40,42 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      //Axios Register
+      axios.post('/api/register', data)
+      .catch(() => toast.error('Something went wrong!'))
+      .finally(() => setIsLoading(false))
     }
 
     if (variant === "LOGIN") {
-      // Next Auth SignIn
+      signIn('credentials', {
+        ...data,
+        redirect: false
+      })
+      .then((callback) => {
+        if(callback?.error) {
+          toast.error('Invalid credentials');
+        }
+
+        if(callback?.ok && !callback?.error) {
+          toast.success("Logged In!")
+        }
+      })
+      .finally(() => setIsLoading(false));
     }
   };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
+    signIn(action, { redirect: false})
+    .then((callback) => {
+      if(callback?.error) {
+        toast.error("Invalid Credentials")
+      }
 
-    //NextAuth Social Sign In
+      if(callback?.ok && !callback?.error) {
+        toast.success("Logged In")
+      }
+    })
+    .finally(() => setIsLoading(false));
   };
 
   return (

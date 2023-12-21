340daffe53aa2f07e5bad92b4c46de958d222ad4
Jalch0 <jlabradorchacon@gmail.com>
2023-12-20 22:05:53 -0400

    Finishing sidebar design

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -3,10 +3,20 @@
 import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+  currentUser
+}) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
+
+  console.log({currentUser})
 
   return (
     <div
@@ -54,6 +64,27 @@ const DesktopSidebar = () => {
             onClick={item.onClick} />
           ))}
         </ul>
+      </nav>
+      <nav
+      className="
+      mt-4
+      flex
+      flex-col
+      justify-between
+      items-center
+      "
+      >
+        <div
+        onClick={() => setIsOpen(true)}
+        className="
+        cursor-pointer
+        hover:opacity-75
+        transition
+        "
+        >
+          <Avatar user={currentUser} />
+        </div>
+
       </nav>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    swcPlugins: [
+      ["next-superjson-plugin", {}]
+    ],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.cloundinary.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

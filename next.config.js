a1d7f95253a999858b1743993bf8564377e556ae
Jalch0 <jlabradorchacon@gmail.com>
2023-12-21 18:42:30 -0400

    new components in users

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "*.cloundinary.com",
+        hostname: "*.cloudinary.com",
         port: "",
         pathname: "**",
       },

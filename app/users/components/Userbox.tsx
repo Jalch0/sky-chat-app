f34456ba4d9610894fd99d0036f200e9ba1978cc
Jalch0 <jlabradorchacon@gmail.com>
2023-12-28 21:01:37 -0400

    Add loading screens with loading modal

--- app/users/components/Userbox.tsx
+++ app/users/components/Userbox.tsx
@@ -1,38 +1,41 @@
-'use client';
+"use client";
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 
 interface UserBoxProps {
-  data: User
+  data: User;
 }
 
-const Userbox: React.FC<UserBoxProps> = ({
-  data
-}) => {
-
+const Userbox: React.FC<UserBoxProps> = ({ data }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = useCallback(() => {
     setIsLoading(true);
 
-    axios.post("/api/conversations", {
-      userId: data.id
-    })
-    .then((data) => {
-      router.push(`/conversations/${data.data.id}`)
-    })
-    .finally(() => setIsLoading(false));
-  }, [data, router])
+    axios
+      .post("/api/conversations", {
+        userId: data.id,
+      })
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => setIsLoading(false));
+  }, [data, router]);
 
   return (
-    <div
-    onClick={handleClick}
-    className="
+    <>
+    {isLoading && (
+      <LoadingModal />
+    )}
+      <div
+        onClick={handleClick}
+        className="
     w-full
     relative
     flex
@@ -45,32 +48,33 @@ const Userbox: React.FC<UserBoxProps> = ({
     transition
     cursor-pointer
     "
-    >
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div
-          className="
+      >
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <div
+              className="
           flex
           justify-between
           items-center
           mb-1
           "
-          >
-            <p
-            className="
+            >
+              <p
+                className="
             text-sm
             font-medium
             text-gray-900
             "
-            >
-              {data.name}
-            </p>
+              >
+                {data.name}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Userbox
+export default Userbox;
